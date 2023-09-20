e64ea3a0f33e34194b87fe13de49387ac9797fde
agnuscrucifixion <nikitabysko123@gmail.com>
2023-09-20 16:46:49 +0300

    create Object

---
+++ src/main/java/com/padwicki/webclinic/domain/entity/Patient.java
@@ -0,0 +1,90 @@
+package com.padwicki.webclinic.domain.entity;
+
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Primary;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @Column(name = "id",nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "serial_number",nullable = false,unique = true)
+    int serialNumber;
+
+    @Column(name = "name",nullable = false)
+    String name;
+
+    @Column(name = "surname",nullable = false)
+    String surname;
+
+    @Column(name = "diagnosis",nullable = false)
+    String diagnosis;
+
+    @Column(name = "coming_date",nullable = false)
+    Timestamp comingDate;
+
+    @Column(name = "prescription_drugs",nullable = false)
+    String drugs;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public Timestamp getComingDate() {
+        return comingDate;
+    }
+
+    public void setComingDate(Timestamp comingDate) {
+        this.comingDate = comingDate;
+    }
+
+    public String getDrugs() {
+        return drugs;
+    }
+
+    public void setDrugs(String drugs) {
+        this.drugs = drugs;
+    }
+}
