e76301083bed7272d5239fa5026ec830b51ed3f6
agnuscrucifixion <nikitabysko123@gmail.com>
2023-09-20 16:57:35 +0300

    part realization of main controller and service

--- src/main/java/com/padwicki/webclinic/controlls/controllers/PatientController.java
+++ src/main/java/com/padwicki/webclinic/controlls/controllers/PatientController.java
@@ -2,6 +2,8 @@ package com.padwicki.webclinic.controlls.controllers;
 
 import com.padwicki.webclinic.controlls.controllersInjection.PatientControllerInterface;
 import com.padwicki.webclinic.domain.entity.Patient;
+import com.padwicki.webclinic.model.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,14 +11,20 @@ import java.util.List;
 @RestController
 public class PatientController implements PatientControllerInterface {
 
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
 
     @Override
     public List<Patient> getPatients() {
-        return null;
+        return patientService.getPatients();
     }
 
     @Override
     public Patient getPatientBySerialNumber(int serialNumber) {
-        return null;
+        return patientService.getPatientBySerialNumber(serialNumber);
     }
 }
--- src/main/java/com/padwicki/webclinic/model/service/PatientService.java
+++ src/main/java/com/padwicki/webclinic/model/service/PatientService.java
@@ -1,21 +1,31 @@
 package com.padwicki.webclinic.model.service;
 
 import com.padwicki.webclinic.domain.entity.Patient;
+import com.padwicki.webclinic.domain.repository.PatientRepository;
 import com.padwicki.webclinic.model.serviceInjection.PatientServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PatientService implements PatientServiceInterface {
 
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
     @Override
     public List<Patient> getPatients() {
-        return null;
+        return patientRepository.findAll();
     }
 
     @Override
     public Patient getPatientBySerialNumber(int serialNumber) {
-        return null;
+        return patientRepository.findPatientBySerialNumber(serialNumber);
     }
 }
