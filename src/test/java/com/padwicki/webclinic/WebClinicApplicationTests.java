de1ff7720908352106e7c331eb835d7773487d7c
agnuscrucifixion <nikitabysko123@gmail.com>
2023-09-20 15:39:14 +0300

    start and create some directories

---
+++ src/test/java/com/padwicki/webclinic/WebClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padwicki.webclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
