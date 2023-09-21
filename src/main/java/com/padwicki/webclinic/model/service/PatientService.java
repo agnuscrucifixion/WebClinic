1808b3b9f53c47cdc562396b6801fcb235d84e56
agnuscrucifixion <nikitabysko123@gmail.com>
2023-09-21 14:52:29 +0300

    add processing exceptions for serial number for example

--- src/main/java/com/padwicki/webclinic/model/service/PatientService.java
+++ src/main/java/com/padwicki/webclinic/model/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.padwicki.webclinic.model.service;
 
+import com.padwicki.webclinic.CustomExceptions.InvalidSerialNumberException;
 import com.padwicki.webclinic.CustomExceptions.NotDoublePatientsException;
 import com.padwicki.webclinic.CustomExceptions.NotFoundPatientException;
 import com.padwicki.webclinic.domain.entity.Patient;
@@ -29,61 +30,87 @@ public class PatientService implements PatientServiceInterface {
 
     @Override
     public Patient getPatientBySerialNumber(int serialNumber) {
-        return patientRepository.findPatientBySerialNumber(serialNumber);
+        if (validateSerialNumber(serialNumber)) {
+            return patientRepository.findPatientBySerialNumber(serialNumber);
+        }
+        throw new InvalidSerialNumberException("Serial number must ne greater than 0");
     }
 
     @Override
     public void addPatient(int serialNumber, String name, String surname,
                            String diagnostic, String drugs) {
-        if (patientRepository.findPatientBySerialNumber(serialNumber) == null) {
-            Patient patient = new Patient();
-            patient.setSerialNumber(serialNumber);
-            patient.setName(name);
-            patient.setSurname(surname);
-            patient.setDiagnosis(diagnostic);
-            patient.setDrugs(drugs);
-            LocalDateTime localDateTime = LocalDateTime.now();
-            patient.setComingDate(Timestamp.valueOf(localDateTime));
-            patientRepository.save(patient);
-        } else {
-            throw new NotDoublePatientsException("A patient with that serial number already EXISTS");
+        if (validateSerialNumber(serialNumber)) {
+            if (patientRepository.findPatientBySerialNumber(serialNumber) == null) {
+                Patient patient = new Patient();
+                patient.setSerialNumber(serialNumber);
+                patient.setName(name);
+                patient.setSurname(surname);
+                patient.setDiagnosis(diagnostic);
+                patient.setDrugs(drugs);
+                LocalDateTime localDateTime = LocalDateTime.now();
+                patient.setComingDate(Timestamp.valueOf(localDateTime));
+                patientRepository.save(patient);
+            } else {
+                throw new NotDoublePatientsException("A patient with that serial number already EXISTS");
+            }
+        } else  {
+            throw new InvalidSerialNumberException("Serial number must ne greater than 0");
         }
+
     }
 
     @Override
     public void updatePatient(int oldSerialNumber, Integer newSerialNumber,
                               String newName, String newSurname,
                               String newDiagnostic, String newDrugs) {
-
-        Patient oldPatient = patientRepository.findPatientBySerialNumber(oldSerialNumber);
-        if (oldPatient != null) {
-            if (newSerialNumber != null) {
-                oldPatient.setSerialNumber(newSerialNumber);
+        if (validateSerialNumber(oldSerialNumber)) {
+            Patient oldPatient = patientRepository.findPatientBySerialNumber(oldSerialNumber);
+            if (oldPatient != null) {
+                if (newSerialNumber != null) {
+                    if (validateSerialNumber(newSerialNumber)) {
+                        oldPatient.setSerialNumber(newSerialNumber);
+                    } else {
+                        throw new InvalidSerialNumberException("Serial number must ne greater than 0");
+                    }
+                }
+                if (newName != null) {
+                    oldPatient.setName(newName);
+                }
+                if (newSurname != null) {
+                    oldPatient.setSurname(newSurname);
+                }
+                if (newDiagnostic != null) {
+                    oldPatient.setDiagnosis(newDiagnostic);
+                }
+                if (newDrugs != null) {
+                    oldPatient.setDrugs(newDrugs);
+                }
+                patientRepository.save(oldPatient);
+            } else {
+                throw new NotFoundPatientException("There is NO patient to change with a given serial number");
             }
-            if (newName != null) {
-                oldPatient.setName(newName);
-            }
-            if (newSurname != null) {
-                oldPatient.setSurname(newSurname);
-            }
-            if (newDiagnostic != null) {
-                oldPatient.setDiagnosis(newDiagnostic);
-            }
-            if (newDrugs != null) {
-                oldPatient.setDrugs(newDrugs);
-            }
-            patientRepository.save(oldPatient);
         } else {
-            throw new NotFoundPatientException("There is NO patient to change with a given serial number");
+            throw new InvalidSerialNumberException("Serial number must ne greater than 0");
         }
     }
 
     @Override
     public void deletePatient(int serialNumber) {
-        Patient patient = patientRepository.findPatientBySerialNumber(serialNumber);
-        if (patient != null) {
-            patientRepository.delete(patient);
+        if (validateSerialNumber(serialNumber)) {
+            Patient patient = patientRepository.findPatientBySerialNumber(serialNumber);
+            if (patient != null) {
+                patientRepository.delete(patient);
+            } else {
+                throw new NotFoundPatientException("There is NO patient to remove with that serial number");
+            }
+        } else  {
+            throw new InvalidSerialNumberException("Serial number must ne greater than 0");
         }
-        throw new NotFoundPatientException("There is NO patient to remove with that serial number");
+
+    }
+
+
+    private boolean validateSerialNumber(int serialNumber) {
+        return serialNumber > 0;
     }
 }
