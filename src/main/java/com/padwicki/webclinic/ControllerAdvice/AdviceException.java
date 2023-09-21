fad1d2061147ff0baf15508ef11fa494d06b84d8
agnuscrucifixion <nikitabysko123@gmail.com>
2023-09-21 14:53:00 +0300

    add handlers for custom and not custom exceptions

--- src/main/java/com/padwicki/webclinic/ControllerAdvice/AdviceException.java
+++ src/main/java/com/padwicki/webclinic/ControllerAdvice/AdviceException.java
@@ -1,12 +1,14 @@
 package com.padwicki.webclinic.ControllerAdvice;
 
 import com.padwicki.webclinic.CustomExceptions.ErrorResponsePatient;
+import com.padwicki.webclinic.CustomExceptions.InvalidSerialNumberException;
 import com.padwicki.webclinic.CustomExceptions.NotDoublePatientsException;
 import com.padwicki.webclinic.CustomExceptions.NotFoundPatientException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 
 @ControllerAdvice
@@ -20,5 +22,20 @@ public class AdviceException {
     public ResponseEntity<ErrorResponsePatient> handleException(NotDoublePatientsException e) {
         ErrorResponsePatient response = new ErrorResponsePatient(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(TypeNotPresentException.class)
+    public ResponseEntity<ErrorResponsePatient> handleException(TypeNotPresentException e) {
+        ErrorResponsePatient response = new ErrorResponsePatient(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(InvalidSerialNumberException.class)
+    public ResponseEntity<ErrorResponsePatient> handleException(InvalidSerialNumberException e) {
+        ErrorResponsePatient response = new ErrorResponsePatient(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponsePatient> handleException(MethodArgumentTypeMismatchException e) {
+        ErrorResponsePatient response = new ErrorResponsePatient("Serial number must ne less than 2147483647");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
