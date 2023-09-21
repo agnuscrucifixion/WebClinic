8426de7c8160596cebc81f0aa1482f979f9318bc
agnuscrucifixion <nikitabysko123@gmail.com>
2023-09-21 18:01:35 +0300

    add JavaDoc description

--- src/main/java/com/padwicki/webclinic/CustomExceptions/ErrorResponsePatient.java
+++ src/main/java/com/padwicki/webclinic/CustomExceptions/ErrorResponsePatient.java
@@ -1,10 +1,14 @@
 package com.padwicki.webclinic.CustomExceptions;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import com.padwicki.webclinic.ControllerAdvice.AdviceException;
 
 /**
  * The class, which will be wrapped in {@link ResponseEntity} and given in this {@link AdviceException}.
  */
+@Getter
+@Setter
 public class ErrorResponsePatient {
     /**
      * Class field that will contain the error message.
@@ -16,22 +20,6 @@ public class ErrorResponsePatient {
      * @param message error message.
      */
     public ErrorResponsePatient(String message) {
-        this.message = message;
-    }
-
-    /**
-     * Getter of {@link ErrorResponsePatient}.
-     * @return error message.
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Setter of {@link ErrorResponsePatient}.
-     * @param message error message.
-     */
-    public void setMessage(String message) {
         this.message = message;
     }
 }
--- src/main/java/com/padwicki/webclinic/domain/entity/Patient.java
+++ src/main/java/com/padwicki/webclinic/domain/entity/Patient.java
@@ -1,93 +1,69 @@
 package com.padwicki.webclinic.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
+/**
+ * An entity class that imitates a patient and stores the main information about the patient.
+ */
+@Getter
+@Setter
 @Entity
 @Table(name = "patients")
 public class Patient {
 
+    /**
+     * Primary key field.
+     */
     @Id
     @Column(name = "id",nullable = false, unique = true)
     @SequenceGenerator(name = "patientsIdSeq", sequenceName = "patients_id_seq", allocationSize = 1, initialValue = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "patientsIdSeq")
     Long id;
 
+    /**
+     * Patient's serial number.
+     * Individual and not repeatable.
+     */
     @Column(name = "serial_number",nullable = false,unique = true)
     int serialNumber;
 
+    /**
+     * Patient's name.
+     */
     @Column(name = "name",nullable = false)
     String name;
 
+    /**
+     * Patient's surname.
+     */
     @Column(name = "surname",nullable = false)
     String surname;
 
+    /**
+     * Patient's diagnosis
+     */
     @Column(name = "diagnosis",nullable = false)
     String diagnosis;
 
+    /**
+     * Date and time the patient was admitted.
+     */
     @Column(name = "coming_date",nullable = false)
     Timestamp comingDate;
 
+    /**
+     *  Drugs for the patient.
+     */
     @Column(name = "prescription_drugs",nullable = false)
     String drugs;
 
+    /**
+     * Empty entity constructor.
+     */
     public Patient() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getSerialNumber() {
-        return serialNumber;
-    }
-
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getDiagnosis() {
-        return diagnosis;
-    }
-
-    public void setDiagnosis(String diagnosis) {
-        this.diagnosis = diagnosis;
-    }
-
-    public Timestamp getComingDate() {
-        return comingDate;
-    }
-
-    public void setComingDate(Timestamp comingDate) {
-        this.comingDate = comingDate;
-    }
-
-    public String getDrugs() {
-        return drugs;
-    }
-
-    public void setDrugs(String drugs) {
-        this.drugs = drugs;
     }
 }
